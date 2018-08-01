9a488b76a61f8682c49700667ae7a4a57f0c0926
志威 <gzw48760@ly.com>
2018-08-01 16:33:49 +0800

    意见反馈初始化

--- app/src/main/java/in/zhiwei/aqi/AQIApplication.java
+++ app/src/main/java/in/zhiwei/aqi/AQIApplication.java
@@ -3,6 +3,7 @@ package in.zhiwei.aqi;
 import android.app.Application;
 import android.content.Intent;
 
+import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
 import com.blankj.utilcode.util.Utils;
 import com.github.promeg.pinyinhelper.Pinyin;
 import com.github.promeg.tinypinyin.lexicons.android.cncity.CnCityDict;
@@ -33,5 +34,20 @@ public class AQIApplication extends Application {
 		//启动定时服务
 		Intent timerIntent = new Intent(this, GetAQIServices.class);
 		startService(timerIntent);
+		initFeedbackService();
+	}
+
+	/**
+	 * 意见反馈的初始化配置
+	 */
+	private void initFeedbackService() {
+		//默认初始化
+		FeedbackAPI.init(this);
+		//沉浸式任务栏，控制台设置为true之后此方法才能生效
+		FeedbackAPI.setTranslucent(true);
+		//设置标题栏"历史反馈"的字号，需要将控制台中此字号设置为0
+		FeedbackAPI.setHistoryTextSize(16);
+		//设置标题栏高度，单位为像素
+		FeedbackAPI.setTitleBarHeight(100);
 	}
 }
